0110761691b89e3b7df51f4aa3fc3fdc3fa9145f
Abderrahim Mama <73607469+abde0103@users.noreply.github.com>
2022-03-08 21:36:13 +0100

    update

--- scripts/MAP583/accuracy_SNR_Batch_norm.sh
+++ scripts/MAP583/accuracy_SNR_Batch_norm.sh
@@ -4,8 +4,8 @@ STR="p_"
 for p1 in 0.11 0.12 0.13 0.135 0.14 0.17 0.3 0.4 0.75
 do 
   cd data/SBMs
-  python generate_SBM.py --p1 $p1 --q 0.1 --size_min 5 --size_max 5 
+  python generate_SBM.py --p1 $p1 --q 0.1  
   cd ../../
   python prepare_data.py --p1 $p1
-  python main_SBMs_node_classification.py --gpu_id 0 --epochs 10 --dataset "$STR$p1$p1" --config "configs/old config/SBMs_GraphTransformer_PATTERN_500k_sparse_graph_BN.json";
+  python main_SBMs_node_classification.py --gpu_id 0 --epochs 50 --dataset "$STR$p1$p1" --config "configs/old config/SBMs_GraphTransformer_PATTERN_500k_sparse_graph_BN.json";
 done
